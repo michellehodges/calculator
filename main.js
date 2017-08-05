29b2677886ca2efa9ad335d8fd59d6895f0d9351
michellehodges <mischamangan@gmail.com>
2017-08-05 18:38:40 -0400

    easy mode almost done...

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const arrayOfNums = document.querySelectorAll(".numbers");
 const arrayOfPemdas = document.querySelectorAll(".pemdas");
 const equalsButton = document.querySelector(".equals");
 let temporaryNumArray = [];
+let temporaryOperatorArray = [];
 let parsedNum;
 let equationArray = [0, ];
 let equationTotal = "";
@@ -19,7 +20,7 @@ for (let i = 0; i <arrayOfPemdas.length; i++) {
   arrayOfPemdas[i].addEventListener('click', function () {
     printToResults(arrayOfPemdas[i].value);
     parseNumAndAddToEquationArray();
-    // assignOperator();
+    assignOperator(arrayOfPemdas[i].value);
   });
 }
 
@@ -30,51 +31,38 @@ equalsButton.addEventListener('click', function () {
 ///////////////////////////FUNCTIONS HERE////////////////////////////////////
 // PRINT BUTTON VALUES WHEN CLICKED
 function printToResults (number) {
-  console.log("printToResults is running")
   let resultsBox = document.querySelector(".results");
   resultsBox.value = resultsBox.value + number;
 }
 
 // // MOVE CLICKED NUMBERS TO TEMPORARY ARRAY; TODO ERROR : i IS NOT DEFINED
 function moveNumsToTempArray (arrayvalue) {
-  console.log("moveNumsToTempArray is running")
   temporaryNumArray.push(arrayvalue)
 }
 
 //WHEN ARRAY OF PEMDAS IS CLICKED 1) parseNumAndAddToEquationArray 3) assignOperator
 function parseNumAndAddToEquationArray () {
-  console.log("parseNumAndAddToEquationArray is running")
   parsedNum = temporaryNumArray.join("");
   equationArray.push(parsedNum);
 }
 
-// CONVERT STRING_NUMBERS TO NUMBERS AND ASSIGN TO NEW VAR USING PARSEINT
-function parseNumAndAddToEquationArray () {
-  // console.log('parseInt will run')
-  Change the numToBeParsed into an integer.
-  Push that value to the array equationArray
+// //ASSIGN OPERATOR TO NEW variable
+function assignOperator(arrayvalue) {
+  equationArray.push(arrayvalue)
 }
 
-// //ASSIGN OPERATOR TO NEW variable
-// function assignOperator() {
-//   // console.log('assignOperator will run')
-//   Everytime arrayOfPemdas[i] is clicked, wait for parseNumAndAddToEquationArray, then push string value of operator to the equationArray.
-// }
-//
-//
-// // RUN THE EQUATION OF VALUES IN resultsBox, PRINT TO resultsBox
-// function runEquation () {
-//   // console.log('runEquation will run')
-//   return `${Number(arr[0]) + parseInt(arr[1] + Number(arr[2]))}`};
-//   switch statement
-//
-//   include possibility of 9+9+= >>> if last value is NaN, then dont push the operator and run function without last operator.
-//   }
-// }
-//
-//
-//
-//
+// RUN THE EQUATION OF VALUES IN resultsBox, PRINT TO resultsBox
+function runEquation () {
+
+  return `${Number(arr[0]) + parseInt(arr[1] + Number(arr[2]))}`};
+  switch statement
+
+  include possibility of 9+9+= >>> if last value is NaN, then dont push the operator and run function without last operator.
+}
+
+
+
+
 
 
 ////////////////////////////////NOTES//////////////////////////////////////////
